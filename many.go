e16e3d29465ea7bb435229ac66e266d00ac65e22
Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
2018-11-05 19:00:17 +1100

    begin repo create and update functions

--- many.go
+++ many.go
@@ -1,8 +1,14 @@
 package main
 
 import (
-	"gopkg.in/alecthomas/kingpin.v2"
+	"errors"
+	"fmt"
 	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
@@ -11,55 +17,51 @@ var (
 		"many",
 		"Microservice versioning tool.",
 	)
-	repo = app.Flag(
+	argRepo = app.Flag(
 		"repo",
-		"Path to the Git repository containing the version file.",
-	).Short('r').Default(".").File()
-	manyFile = app.Flag(
+		"Path to the Many repository.",
+	).Short('r').Default(".").String()
+	argFile = app.Flag(
 		"file",
-		"Path to the file containing the version information.",
-	).Short('f').Default("./many.toml").String()
-
-	initialise = app.Command(
+		"Name of the Many file.",
+	).Short('f').Default("Many.toml").String()
+	initial = app.Command(
 		"init",
-		"Initialize a new Many Git repository with an empty versioning file. "+
+		"Initialize a new Many repository with an empty versioning file. "+
 			"If a repository exists at the provided URL then it is cloned.",
 	)
-	initialiseUpdate = initialise.Flag(
+	initialUpdate = initial.Flag(
 		"update",
-		"Update Many Git repository details if it is already initialised.",
+		"Update Many repository details if it is already initialised.",
 	).Short('u').Default("false").Bool()
-	initialiseName = initialise.Arg(
+	initialName = initial.Arg(
 		"name",
-		"Name of the Many Git repository.",
+		"Name of the Many repository.",
 	).Required().String()
-	initialiseRemoteUrl = initialise.Arg(
+	initialRemoteURL = initial.Arg(
 		"git-url",
 		"URL of the Git remote.",
-	).URL()
-	initialiseRemoteName = initialise.Flag(
+	).Required().String()
+	initialRemoteName = initial.Flag(
 		"remote",
 		"Name of the Git remote.",
 	).Short('m').Default("origin").String()
-	initialiseVersion = initialise.Flag(
+	initialVersion = initial.Flag(
 		"inital-version",
 		"The starting version.",
 	).Short('i').Default("0.0.0").String()
-	initialiseNoClone = initialise.Flag(
+	initialNoClone = initial.Flag(
 		"no-clone",
 		"Do not clone the from an existing repository at the remote URL.",
 	).Short('n').Default("false").Bool()
-
 	pull = app.Command(
 		"pull",
-		"Pull changes from the remote Many Git repository.",
+		"Pull changes from the remote Many repository.",
 	)
-
 	push = app.Command(
 		"push",
-		"Push changes to the remote Many Git repository.",
+		"Push changes to the remote Many Grepository.",
 	)
-
 	create = app.Command(
 		"create",
 		"Register a new microservice with Many.",
@@ -79,12 +81,11 @@ var (
 	createGit = create.Flag(
 		"git",
 		"URL of the Git repository for the microservice.",
-	).Short('g').URL()
+	).Short('g').String()
 	createDocker = create.Flag(
 		"docker",
 		"URL of the Docker repository for the microservice.",
 	).Short('c').URL()
-
 	view = app.Command(
 		"view",
 		"View details for microservices.",
@@ -93,7 +94,6 @@ var (
 		"services",
 		"CSV list of microservices.",
 	).Required().String()
-
 	delete = app.Command(
 		"delete",
 		"Delete a microservice.",
@@ -102,7 +102,6 @@ var (
 		"service",
 		"Name of microservice.",
 	).Required().String()
-
 	promote = app.Command(
 		"promote",
 		"Promote a candidate version of a microservice.",
@@ -115,12 +114,10 @@ var (
 		"version",
 		"Candidate version.",
 	).Required().String()
-
 	current = app.Command(
 		"current",
 		"View the current overall version.",
 	)
-
 	release = app.Command(
 		"release",
 		"Create a new overall version from the candidates.",
@@ -131,15 +128,144 @@ var (
 	).Required().Enum("patch", "minor", "major")
 )
 
+type ManyVersion struct {
+	version     string
+	date        time.Time
+	description string
+}
+
+type ManyService struct {
+	name        string
+	description string
+	git         string
+	docker      string
+	candidate   ManyVersion
+	versions    []ManyVersion
+}
+
+type ManyFile struct {
+	name       string
+	remoteURL  string `toml:"remote_url"`
+	remoteName string `toml:"remote_name"`
+	versions   []ManyVersion
+	services   map[string]ManyService
+}
+
+type ManyRepo struct {
+	path     string
+	file     string
+	manyFile ManyFile
+}
+
+func (orig *ManyService) update(changes ManyService) error {
+	orig.name = changes.name
+	orig.description = changes.description
+	orig.git = changes.git
+	orig.docker = changes.docker
+	orig.candidate = changes.candidate
+	// TODO check merged versions
+	// newest first
+	orig.versions = append(changes.versions, orig.versions...)
+	return nil
+}
+
+func (orig *ManyFile) update(changes ManyFile) error {
+	orig.name = changes.name
+	orig.remoteURL = changes.remoteURL
+	orig.remoteName = changes.remoteName
+	// TODO check merged versions
+	// newest first
+	orig.versions = append(changes.versions, orig.versions...)
+	for name, service := range changes.services {
+		origService, ok := orig.services[name]
+		if ok {
+			origService.update(service)
+		}
+	}
+	return nil
+}
+
+func (orig *ManyRepo) update(changes ManyRepo) error {
+	orig.path = changes.path
+	orig.file = changes.file
+	err := orig.manyFile.update(changes.manyFile)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LoadManyRepo(repoPath string, filePath string) (*ManyRepo, error) {
+	repoPath = filepath.Clean(repoPath)
+	filePath = filepath.Join(repoPath, filePath)
+	_, err := os.Stat(repoPath)
+	if err != nil {
+		return nil, err
+	}
+	_, err = os.Stat(filePath)
+	if err != nil {
+		return nil, err
+	}
+	var manyFile *ManyFile
+	_, err = toml.DecodeFile(filePath, manyFile)
+	if err != nil {
+		return nil, err
+	}
+	return &ManyRepo{
+		path:     repoPath,
+		file:     filePath,
+		manyFile: *manyFile,
+	}, nil
+}
+
+func CreateManyRepo(
+	repoPath string,
+	filePath string,
+	name string,
+	remoteURL string,
+	remoteName string,
+	update bool,
+	noClone bool,
+) (*ManyRepo, error) {
+	repo := ManyRepo{
+		path: repoPath,
+		file: filePath,
+		manyFile: ManyFile{
+		  name:       name,
+		  remoteURL:  remoteURL,
+		  remoteName: remoteName,
+		  versions:   []ManyVersion{},
+		  services:   map[string]ManyService{},
+	  },
+	}
+	origRepo, err := LoadManyRepo(repoPath, filePath)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return nil, err
+		}
+		if !update {
+			return nil, errors.New("Repository already exists. Use --update to update it.")
+		}
+		err = origRepo.update(repo)
+		if err != nil {
+			return nil, err
+		}
+		return origRepo, nil
+	}
+	// TODO save
+	return &repo, nil
+}
+
 func main() {
 	app.HelpFlag.Short('h')
 	app.Version(version)
 	app.VersionFlag.Short('v')
 	command := kingpin.MustParse(app.Parse(os.Args[1:]))
+	fmt.Println("Started.")
 
 	switch command {
 	case "init":
-		// TODO
+		fmt.Println("Initialising Many repo.")
 
 	case "pull":
 		// TODO
@@ -162,4 +288,6 @@ func main() {
 	case "increment":
 		// TODO
 	}
+
+	fmt.Println("Success.")
 }
